40dcb0ffba6e66f46f5ceb37ea8870ffeb2a75bb
Swayangdipta Das <swayangdiptacc@gmail.com>
2022-11-29 11:11:22 +0530

    Added Full Todo Update Feature

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from 'react'
 import {MdExpandMore} from 'react-icons/md'
-import {AiFillDelete,AiTwotoneEdit} from 'react-icons/ai'
+import {AiFillDelete,AiTwotoneEdit,AiOutlineClose,AiOutlineLoading3Quarters} from 'react-icons/ai'
 import {BiCheckDouble} from 'react-icons/bi'
 import {FaCheck} from 'react-icons/fa'
 import { editTodo, removeTodo } from '../utils/TodoApiCalls'
@@ -11,8 +11,17 @@ import { TodoContext } from '../context/TodoContext'
 const Todo = ({todo}) => {
     const [todos,setTodos] = useContext(TodoContext)
     const [isExpanded,setIsExpanded] = useState(false)
+    const [isEditOpen,setIsEditOpen] = useState(false)
     const {user,token} = isAuthenticated()
+    const [updateInputs,setUpdateInputs] = useState({
+        title: todo.title,
+        tasks: todo.tasks,
+        isCompleted: todo.isCompleted,
+        task: ''
+    })
+    const [isLoading,setIsLoading] = useState(false)
 
+    const {title,task,tasks} = updateInputs
 
     const handleTodoDelete = e => {
         e.preventDefault()
@@ -34,7 +43,7 @@ const Todo = ({todo}) => {
 
     const editThisTodo = (type,item) => {
         if(type === "all"){
-
+            setIsEditOpen(true)
         }else if(type === "status"){
             todo.isCompleted = true
             let newTodos = todos.map(t => {
@@ -61,15 +70,60 @@ const Todo = ({todo}) => {
     }
 
     const separatedEditTodo = () => {
+        setIsLoading(true)
         editTodo(todo._id,user._id,token,todo).then(response => {
             if(!response?.response?.data?.error){
                 toast.success("Todo updated.",{theme: "colored"})
+                setIsLoading(false)
+                return true
             }else{
                 toast.error(response?.response?.data?.error,{theme: "colored"})
+                setIsLoading(false)
+                return false
             }
         }).catch(e=>{
-            return toast.error(e,{theme: "colored"})
+            toast.error(e,{theme: "colored"})
+            setIsLoading(false)
+            return false
         })
+    }
+
+    const handleChange = (field,e) => {
+        setUpdateInputs({...updateInputs,[field]: e.target.value})
+    }
+
+    const addTask = e => {
+        e.preventDefault()
+        if(task === ''){
+            toast.error("Cannot add empty task!",{theme: "colored"})
+            return false
+        }
+        setUpdateInputs({...updateInputs,tasks: [...tasks,task],task: ''})
+    }
+
+    const handleTaskRemove = (taskToRemove,e) => {
+        e.preventDefault()
+        setUpdateInputs(prev => {
+           return {...prev,tasks: tasks.filter(t => t !== taskToRemove)}
+        })
+    }
+
+    const handleUpdate = (e) => {
+        e.preventDefault()
+        todo.title = title;
+        todo.tasks = tasks
+        
+        if(separatedEditTodo()){
+            let newTodos = todos.map(t => {
+                if(t._id === todo._id){
+                    return todo
+                }else{
+                    return t
+                }
+            })
+            setTodos(newTodos)
+            setIsEditOpen(false)
+        }
     }
 
   return (
@@ -111,6 +165,40 @@ const Todo = ({todo}) => {
                             </div>
                         ))
                     }
+                </div>
+            )
+        }
+        {
+            isEditOpen && (
+                <div className='z-50 w-screen h-screen fixed top-0 left-0 flex items-center justify-center backdrop-blur-sm'>
+                    <div className='dark:bg-slate-800 shadow-xl rounded-md bg-purple-600 w-[500px] min-h-[300px] max-h-max pb-[20px]'>
+                        <div className='dark:bg-black bg-purple-900 rounded-t-md flex items-center justify-between h-[50px]'>
+                            <h2 className='text-[20px]'>Update</h2>
+                            <div onClick={e=>setIsEditOpen(false)} className=' cursor-pointer flex items-center justify-center w-[30px] h-[30px] rounded-full bg-rose-600 text-[20px] mr-[10px]'><AiOutlineClose /></div>
+                        </div>
+                        <form className='w-[100%] flex justify-center gap-[20px]'>
+                        <div className="formLeft flex flex-col w-[90%] mt-[20px]">
+                            <input value={title} onChange={e=>handleChange("title",e)} type="text" placeholder='Type your todo here...' className='outline-none rounded-md indent-[10px] h-[30px] text-black' />
+                            <div className='flex w-[100%] gap-[7px]'>
+                            <input value={task} onChange={e=>handleChange("task",e)} type="text" placeholder='Type your tasks here...' className='outline-none rounded-md indent-[10px] w-[100%] h-[30px] mt-[10px] text-black'/>                
+                            <button className='w-[150px] h-[30px] mt-[10px] flex items-center justify-center dark:bg-slate-700 dark:text-slate-200 bg-lime-400 font-[500] text-[#371458] rounded-md' onClick={addTask}>Add task</button>
+                            </div>
+                        </div>
+                        </form>
+
+                        <div className='w-full max-h-[200px] mt-[30px] overflow-scroll overflow-x-hidden'>
+                            {
+                                tasks.length > 0 && tasks.map((task,index)=>(
+                                    <div key={index} className="task mx-auto mt-[5px] w-[90%] h-max p-[5px] pr-[0px] dark:bg-zinc-700 bg-purple-800 rounded-md flex items-center justify-between gap-[10px] text-white relative">
+                                        <p>{task}</p>
+                                        <div onClick={e=>handleTaskRemove(task,e)} className='cursor-pointer rounded-r-md hover:bg-rose-800 h-full w-[30px] flex items-center justify-center bg-rose-600 absolute top-0 right-0'><AiOutlineClose /></div>
+                                    </div>
+                                ))
+                            }
+                        </div>
+
+                        <button onClick={handleUpdate} className='w-[90%] h-[40px] mx-[5%] mt-[10px] rounded-md dark:bg-black bg-purple-900 outline-none flex items-center justify-center'>{isLoading ? (<AiOutlineLoading3Quarters className='animate-spin' />) : ("Update Todo")}</button>
+                    </div>
                 </div>
             )
         }
